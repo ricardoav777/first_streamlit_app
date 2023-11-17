23a8f8cbb97376644aedad8327030ade357233b5
Ricardo <rialfaro98@gmail.com>
2023-11-16 21:25:21 -0600

    Update streamlit_app.py normalize
    normalize an df

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+
+# converts json format into normal text
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# convert normal text into a dataframe
+streamlit.dataframe(fruityvice_normalized)
