ef7a43976ea7ea80ec169c8347630be81f3711b3
Ricardo <rialfaro98@gmail.com>
2023-11-16 23:01:00 -0600

    added new structure and functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -22,23 +26,27 @@ fruits_to_show = my_fruit_list.loc[fruits_select]
 streamlit.dataframe(fruits_to_show)
 
 
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  # converts json format into normal text
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 # new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
-import requests
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# converts json format into normal text
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# convert normal text into a dataframe
-streamlit.dataframe(fruityvice_normalized)
-
-import snowflake.connector
-
-
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("PLease select a fruit to get information.")
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    # convert normal text into a dataframe
+    streamlit.dataframe(fruityvice_normalized)
+    
+except URLError as e:
+  streamlit.error()
+  
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
