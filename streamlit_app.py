2f87eb26605249d1096c34ed084cfa16c4c88842
Ricardo <rialfaro98@gmail.com>
2023-11-16 21:37:16 -0600

    Update  fruit choice
    ADDING  fruit choice to request

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # converts json format into normal text
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
